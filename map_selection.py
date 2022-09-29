950701f45e2939b59cde207c57dcf7e170a17da3
Joachim Brendborg <Joachim@brendborg.dk>
2022-09-29 13:50:48 +0200

    Created a map selection where we can select the time and it shows the frequency on the map

--- map_selection.py
+++ map_selection.py
@@ -4,29 +4,30 @@ import pandas as pd
 
 
 # df = pd.read_csv("2022928135943390963562STRAF1150490172666.csv")
-# df = df.rename(columns={' .1' : 'label_dk'})
-# df = df.drop(' ', axis=1)
-df = pd.read_csv("crime_data.csv", encoding='utf-8')
 
-print(df)
-
-# # load raw url in geodataframe
-# url = 'https://raw.githubusercontent.com/magnuslarsen/geoJSON-Danish-municipalities/master/municipalities/municipalities.geojson'
-# gdf = gpd.read_file(url)
-
-# # Merge the data frames
-# data = gdf.merge(df, how='left', on='label_dk')
-# data = data.fillna(0)
-
-# # define inline geojson data object
-# data_geojson = alt.InlineData(values=gdf.to_json(), format=alt.DataFormat(property='features',type='json')) 
+df = pd.read_csv("crime_data.csv", encoding="iso-8859-1")
+df = df.rename(columns={' .1' : 'label_dk'})
+df = df.drop(' ', axis=1)
 
 
+# load raw url in geodataframe
+url = 'https://raw.githubusercontent.com/magnuslarsen/geoJSON-Danish-municipalities/master/municipalities/municipalities.geojson'
+gdf = gpd.read_file(url)
+
+# Merge the data frames
+data = gdf.merge(df, how='left', on='label_dk')
+data = data.fillna(0)
+
+# define inline geojson data object
+data_geojson = alt.InlineData(values=gdf.to_json(), format=alt.DataFormat(property='features',type='json')) 
+
+columns = df.columns.to_list()[1:]
+
+time_dropdown = alt.binding_select(options=columns, name='Time')
+time_selection = alt.selection_single(fields=['Time'], bind=time_dropdown)
 # single = alt.selection_single()
 
-# # chart object
-# char = alt.Chart(data).mark_geoshape().encode(
-#     color=alt.condition(
+# color=alt.condition(
 #         single, 
 #         alt.Color(
 #             "2007K1:Q",
@@ -34,10 +35,19 @@ print(df)
 #                 scheme='viridis')
 #         ), 
 #         alt.value('lightgray'))
-# ).add_selection(
-#     single
-# ).properties(
-#     width=500,
-#     height=300
-# )
-# char.show()
+
+# chart object
+char = alt.Chart(data).transform_fold(
+    columns,
+    as_=['Time', 'value']
+).transform_filter(
+    time_selection  
+).mark_geoshape().encode(
+    color='value:Q'
+).add_selection(
+    time_selection
+).properties(
+    width=500,
+    height=300
+)
+char.show()
