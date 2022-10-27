e28614cf45ba1a538ec90b37c5d7e8d938702cae
Asger Ullersted Rasmussen <201806934@post.au.dk>
2022-10-27 10:19:10 +0200

    tried to fix bad commits

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,18 +1,19 @@
 import geopandas as gpd
 import pandas as pd
 
-file = "small_umbrella_terms_crimes"
-
+file = "umbrella_terms_crimes"
+"""
 url = 'https://raw.githubusercontent.com/magnuslarsen/geoJSON-Danish-municipalities/master/municipalities/municipalities.geojson'
 gdf = gpd.read_file(url)
-gdf = gdf[['label_dk','geometry']]
+print(gdf.columns)
+gdf['type'] = "Feature"
+gdf = gdf[['label_dk','geometry','type']]
 gdf.loc[gdf.label_dk == 'Århus', 'label_dk'] = "Aarhus"
 gdf.loc[gdf.label_dk == 'Brønderslev-Dronninglund', 'label_dk'] = "Brønderslev"
 gdf.loc[gdf.label_dk == 'Vesthimmerland', 'label_dk'] = "Vesthimmerlands"
 gdf = gdf.dissolve(by='label_dk')
-
-
-
+gdf.to_file("data_with_geo/" + "geometry" + ".geojson", driver='GeoJSON')
+"""
 
 df = pd.read_csv("data/" + file + ".csv", encoding="utf_8")
 df = df.rename(columns={'område': 'label_dk', "overtrædelsens art": "offence"})
@@ -20,5 +21,13 @@ df = df.astype({'Anmeldte forbrydelser': 'int32'})
 #df['tid'] = df['tid'].apply(lambda x : x[:-2])
 #df = df.groupby(['tid','offence','label_dk'])['Anmeldte forbrydelser'].sum().reset_index()
 
-big_fuck_df = gdf.merge(df, how='left', on='label_dk')
-big_fuck_df.to_file("data_with_geo/" + file + "_quarters.geojson", driver='GeoJSON')
+pop_df = pd.read_csv("data/populations.csv", encoding="utf_8")
+pop_df = pop_df.rename(columns={'område': 'label_dk','Folketal den 1. i kvartalet':'population'})
+df = df.merge(pop_df, how="inner", on=['label_dk', 'tid'])
+#df.to_csv("data/"+file + "_pop" + ".csv")
+file = file + "_pop"
+df.to_csv("data/"+ file + ".csv")
+
+
+#big_fuck_df = gdf.merge(df, how='left', on='label_dk')
+#big_fuck_df.to_file("data_with_geo/" + file + "_quarters.geojson", driver='GeoJSON')
